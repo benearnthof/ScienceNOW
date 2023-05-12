45c10d8e1f3245329fbd75d7caba270cf9cda211
unknown <benearnthof@hotmail.de>
2023-05-12 14:25:06 +0200

    first commit with FastAPI outline

--- sciencenow/sources/arxivsource.py
+++ sciencenow/sources/arxivsource.py
@@ -9,38 +9,40 @@ from urllib.parse import urlencode
 
 from base import Source
 
+
 class ArxivSource(Source):
     """
-    Uses feedparser library to retrieve posts from the arxiv.com RSS feed. 
+    Uses feedparser library to retrieve posts from the arxiv.com RSS feed.
     """
-    def __init__(self, base_url='http://export.arxiv.org/api/query?'):
-      """
-      Initializes the base_url attribute with the arxiv.com API endpoint.
-      """
+
+    def __init__(self, base_url="http://export.arxiv.org/api/query?"):
+        """
+        Initializes the base_url attribute with the arxiv.com API endpoint.
+        """
         self.base_url = base_url
-    
+
     def get_posts(self, keywords=None, since=None, start=0, num=100):
         """
         Retrieve a fixed number of recent posts from arxiv.com.
         Constructs the query URL based on the provided criteria, retrieves the feed using feedparser.parse,
-        and extracts relevant post information from the feed entries. 
+        and extracts relevant post information from the feed entries.
         """
         # Construct query parameters
         query_params = {
-            'search_query': 'all:{}'.format(keywords) if keywords else 'all',
-            'start': start,
-            'max_results': num,
-            'sortBy': 'lastUpdatedDate',
-            'sortOrder': 'descending',
-            'timeframe': 'last_7_days' if since is None else 'last_30_days',
+            "search_query": "all:{}".format(keywords) if keywords else "all",
+            "start": start,
+            "max_results": num,
+            "sortBy": "lastUpdatedDate",
+            "sortOrder": "descending",
+            "timeframe": "last_7_days" if since is None else "last_30_days",
         }
         if since:
-            query_params['since'] = since.strftime('%Y-%m-%d')
-        
+            query_params["since"] = since.strftime("%Y-%m-%d")
+
         # Construct query URL and retrieve feed
         url = self.base_url + urlencode(query_params)
         feed = feedparser.parse(url)
-        
+
         # Extract relevant post information
         # TODO: Change this to return posts in Database format
         posts = []
@@ -48,40 +50,44 @@ class ArxivSource(Source):
             title = entry.title
             link = entry.link
             summary = entry.summary
-            published = datetime.datetime.strptime(entry.published, '%Y-%m-%dT%H:%M:%SZ')
-            updated = datetime.datetime.strptime(entry.updated, '%Y-%m-%dT%H:%M:%SZ')
-            posts.append({
-                'title': title,
-                'link': link,
-                'summary': summary,
-                'published': published,
-                'updated': updated,
-            })
-        
+            published = datetime.datetime.strptime(
+                entry.published, "%Y-%m-%dT%H:%M:%SZ"
+            )
+            updated = datetime.datetime.strptime(entry.updated, "%Y-%m-%dT%H:%M:%SZ")
+            posts.append(
+                {
+                    "title": title,
+                    "link": link,
+                    "summary": summary,
+                    "published": published,
+                    "updated": updated,
+                }
+            )
+
         return posts
-    
+
     def fetch_new(self):
         """
         Retrieve all new posts since the last time this method was run.
-        Gets the last fetch time from the database, sets the since parameter in the get_posts method to the last fetch time plus one second, 
-        obtains new posts using get_posts, updates the database with the new posts, and updates the last fetch time in the database to the current time. 
+        Gets the last fetch time from the database, sets the since parameter in the get_posts method to the last fetch time plus one second,
+        obtains new posts using get_posts, updates the database with the new posts, and updates the last fetch time in the database to the current time.
         """
         # Get last fetch time from database
-        last_fetch_time = # Get last fetch time from database
-        
+        last_fetch_time = 0  # TODO: Get last fetch time from database
+
         # Set since parameter to last fetch time
         since = last_fetch_time + datetime.timedelta(seconds=1)
-        
+
         # Retrieve new posts and update database
         new_posts = self.get_posts(since=since)
         # Update database with new posts
-        
+
         # Update last fetch time in database
         current_time = datetime.datetime.now()
         # Update last fetch time in database to current_time
-        
+
         return new_posts
-    
+
     def fetch_all(self):
         """
         Retrieve all posts from arxiv.com.
@@ -90,5 +96,5 @@ class ArxivSource(Source):
         # Retrieve all posts and update database
         all_posts = self.get_posts()
         # TODO: Update database with all posts
-        
+
         return all_posts
--- sciencenow/sources/base.py
+++ sciencenow/sources/base.py
@@ -4,21 +4,22 @@ Abstract base classes for all source objects
 
 from abc import ABCMeta, abstractmethod
 
+
 class Source(metaclass=ABCMeta):
-  @abstractmethod
-  def get_posts(self, keywords=None, since=None, start=0, num=100)
-    """
-    Get a fixed number of recent posts.
-    """
-    
-  @abstractmethod
-  def fetch_new(self):
-    """
-    Fetch all new posts since this method was last run.
-    """
-    
-  @abstractmethod
-  def fetch_all(self):
-    """
-    Fetch all resources needed to instantiate the database.
-    """
+    @abstractmethod
+    def get_posts(self, keywords=None, since=None, start=0, num=100):
+        """
+        Get a fixed number of recent posts.
+        """
+
+    @abstractmethod
+    def fetch_new(self):
+        """
+        Fetch all new posts since this method was last run.
+        """
+
+    @abstractmethod
+    def fetch_all(self):
+        """
+        Fetch all resources needed to instantiate the database.
+        """
