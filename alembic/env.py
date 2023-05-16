3f147984e3d7a72596401eca2ada7625f0e84f7e
unknown <benearnthof@hotmail.de>
2023-05-16 16:12:22 +0200

    add models to alembic env

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,9 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from sciencenow.db.db_setup import Base
+from sciencenow.db.db_models import ArxivModel
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -18,7 +21,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -64,9 +67,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
