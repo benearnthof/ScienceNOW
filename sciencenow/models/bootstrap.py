671868729c68274b8472ecf135b5410098fd7850
benearnthof <benearnthof@hotmail.de>
2023-11-06 12:21:34 +0100

    Update bootstrap.py

--- sciencenow/models/bootstrap.py
+++ sciencenow/models/bootstrap.py
@@ -23,7 +23,7 @@ from pathlib import Path
 # Step 0: Gridsearch to compare impact of hyperparameters on the number of cluster and outliers that are 
 # generated
 
-sentence_model = SentenceTransformer("all-MiniLM-L6-v2")
+# sentence_model = SentenceTransformer("all-MiniLM-L6-v2")
 # precompute embeddings
 # embeddings_2020 = embedding_model.encode(docs_2020, show_progress_bar=True)
 
@@ -31,24 +31,24 @@ sentence_model = SentenceTransformer("all-MiniLM-L6-v2")
 # n_components: 2:20
 # min_cluster_size: 10, 20, 30, ..., 250
 # TODO: evaluate scores => obtain corpus and vocab needed for octis etc. 
-nbors = range(10, 20, 2)
+# nbors = range(10, 20, 2)
 # for anything more than 10 components the clustering saturates at around 65 clusters
 # 5 neighbors and min cluster size of 100
-ncomp = range(5, 11, 2) 
-mclust = range(25, 300, 25)
+# ncomp = range(5, 11, 2) 
+# mclust = range(25, 300, 25)
 
 len(nbors) * len(ncomp) * len(mclust) * 50 / 3600
 from bertopic import BERTopic
 
-params = {
-    "verbose": True,
-    "umap_model": UMAP(n_neighbors = 15, n_components=5, metric='cosine', low_memory=False, random_state=42),
-    "hdbscan_model": HDBSCAN(min_cluster_size=1000, metric='euclidean', prediction_data=True),
-    "ctfidf_model": ClassTfidfTransformer(reduce_frequent_words=True)
-}
+# params = {
+#     "verbose": True,
+#     "umap_model": UMAP(n_neighbors = 15, n_components=5, metric='cosine', low_memory=False, random_state=42),
+#     "hdbscan_model": HDBSCAN(min_cluster_size=1000, metric='euclidean', prediction_data=True),
+#     "ctfidf_model": ClassTfidfTransformer(reduce_frequent_words=True)
+# }
 
 
-tm = BERTopic(**params)
+# tm = BERTopic(**params)
 
 from sentence_transformers import SentenceTransformer
 from ScienceNOW.sciencenow.models.trainer import Trainer
@@ -61,47 +61,64 @@ data, timestamps = data_loader.load_docs()
 #data, timestamps = docs, timestamps
 # timestamps = [str(stamp) for stamp in timestamps]
 # model = SentenceTransformer("all-mpnet-base-v2")
-# sentence_model = SentenceTransformer("all-MiniLM-L6-v2")
+sentence_model = SentenceTransformer("all-MiniLM-L6-v2")
 #_, timestamps = data_loader.load_docs()
 #
 
 embeddings = sentence_model.encode(data, show_progress_bar=True)
 
-topics, _ = tm.fit_transform(data, embeddings)
+# precompute reduced embeddings
+class Dimensionality:
+    """ Use this for pre-calculated reduced embeddings """
+    def __init__(self, reduced_embeddings):
+        self.reduced_embeddings = reduced_embeddings
+    def fit(self, X):
+        return self
+    def transform(self, X):
+        return self.reduced_embeddings
+
+from umap import UMAP
+umap_model = UMAP(n_neighbors = 15, n_components=5, metric='cosine', low_memory=False, random_state=42)
+reduced_embeddings = umap_model.fit_transform(embeddings)
+
+
+# topics, _ = tm.fit_transform(data, embeddings)
 
 custom_path = Path("/dss/dssmcmlfs01/pr74ze/pr74ze-dss-0001/ru25jan4/ScienceNOW/artifacts/bootstrap")
 
 # Extract vocab to be used in BERTopic
 from collections import Counter
-docs = data
-vocab = Counter()
-tokenizer = CountVectorizer().build_tokenizer()
-for doc in tqdm(docs):
-     vocab.update(tokenizer(doc))
+# docs = data
+# vocab = Counter()
+# tokenizer = CountVectorizer().build_tokenizer()
+# for doc in tqdm(docs):
+#      vocab.update(tokenizer(doc))
 
-with open(custom_path / "vocab.txt", "w") as file:
-    for item in vocab:
-        file.write(item+"\n")
-file.close()
+# with open(custom_path / "vocab.txt", "w") as file:
+#     for item in vocab:
+#         file.write(item+"\n")
+# file.close()
 
-docs = [doc.replace("\n", " ") for doc in docs]
-assert all("\n" not in doc for doc in docs)
+# docs = [doc.replace("\n", " ") for doc in docs]
+# assert all("\n" not in doc for doc in docs)
 
-with open(custom_path / "corpus.tsv", "w") as file:
-    for document in docs:
-        file.write(document + "\n")
-file.close()
+# with open(custom_path / "corpus.tsv", "w") as file:
+#     for document in docs:
+#         file.write(document + "\n")
+# file.close()
 
 # now run every set of parameters three times to help combat randomness introduced by UMAP
+# we analyze the influence of the number of components on the result aswell since ideally
+# we would like to precompute the reduced embeddings for faster caching and performance
 from tqdm import tqdm
 results = []
-len(range(10,21,1)) * len(range(25, 325, 25)) * 60 / 3600
-for neigh in tqdm(range(10,21,1)):
-    for clust in range(25, 325, 25):
+len(range(1,50,5)) * len(range(25, 325, 25)) * 60 / 3600
+for clust in tqdm(range(25, 525, 25)):
+    for samp in tqdm(range(1, 50, 5)): # min_samples has a negligible impact on diversity and npmi
         params = {
         "verbose": False,
-        "umap_model": UMAP(n_neighbors = neigh, n_components=5, metric='cosine', low_memory=False, random_state=42),
-        "hdbscan_model": HDBSCAN(min_cluster_size=clust, metric='euclidean', prediction_data=True),
+        "umap_model": Dimensionality(reduced_embeddings),
+        "hdbscan_model": HDBSCAN(min_cluster_size=clust, min_samples=samp, metric='euclidean', prediction_data=True),
         "ctfidf_model": ClassTfidfTransformer(reduce_frequent_words=True)
         }
         trainer = Trainer(dataset=dataset,
@@ -116,7 +133,15 @@ for neigh in tqdm(range(10,21,1)):
         res = trainer.train()
         results.append(res)
 
+#import pickle
 
+with open(custom_path / "results.txt", "w") as file:
+    file.write("\n".join(str(item) for item in results))
+file.close()
+
+with open(custom_path / "results.pkl", "rb") as file: 
+    results = pickle.load(file)
+file.close()
 
 
 # 8640 * 50 / 3600 120 hours => need smaller gridsearch
